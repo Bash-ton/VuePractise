54e253f37fbe3f714450cf61c274aa0644f137c1
Bash-ton <sebbepaz@gmail.com>
2021-04-01 18:17:04 +0200

    initial vue practise, tutorial #4 done

---
+++ modal-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">Hello Vue</div>
+  <h1>{{ title }}</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "Practising Vue",
+    };
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
